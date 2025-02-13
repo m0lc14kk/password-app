3cad1db8d013c51aeab448019f8376f45a90c533
m0lc14kk <m0lc14kk@outlook.com>
2025-02-13 14:09:35 +0100

    feat: dodano skrypt od automatycznego tworzenia bazy danych

--- src/globals/AppDatabase.ts
+++ src/globals/AppDatabase.ts
@@ -1,7 +1,13 @@
 import sqlite3 from "sqlite3";
 import { join } from "path";
+import { existsSync, writeFileSync } from "node:fs";
 
-const appDatabase = new sqlite3.Database(join(process.cwd(), "databases/database.db"));
+const filePath: string = join(process.cwd(), "databases/database.db");
+if (!existsSync(filePath)) {
+    writeFileSync(filePath, "");
+}
+
+const appDatabase = new sqlite3.Database(filePath);
 appDatabase.exec(`
     CREATE TABLE IF NOT EXISTS passwords (
         id          INTEGER         PRIMARY KEY AUTOINCREMENT,
