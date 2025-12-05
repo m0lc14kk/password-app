76d6c59cd23a799dabfd882f97931c274a8ec72a
Oliwier <m0lc14kk@outlook.com>
2025-12-05 17:46:01 +0100

    feat(window): added basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+#include <QApplication>
+#include <QMainWindow>
+#include <QLabel>
 
-int main()
-{
-    std::cout << "Hello World" << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QMainWindow window;
+    window.setWindowTitle("Hello World");
+    window.resize(400, 300);
+
+    QLabel *label = new QLabel("Hello World!", &window);
+    label->setAlignment(Qt::AlignCenter);
+    window.setCentralWidget(label);
+
+    window.show();
+
+    return app.exec();
 }
