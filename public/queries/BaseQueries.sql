61cc175fa50359231c08ab454ee259607854f65c
Oliwier <m0lc14kk@outlook.com>
2025-12-06 14:08:10 +0100

    feat(query): added base queries for accounts and passwords

---
+++ public/queries/BaseQueries.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(36) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    encryptedSecurityPin VARCHAR(255) NOT NULL,
+    avatarId INT NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS passwords (
+    id VARCHAR(36) NOT NULL,
+    accountId VARCHAR(36) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    encryptedPassword VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (accountId) REFERENCES accounts (id) ON DELETE CASCADE
+);
