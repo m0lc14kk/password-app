1ede639f75460b414841917d007c8aaa81393482
Oliwier <m0lc14kk@outlook.com>
2025-12-06 09:59:49 +0100

    feat(scripts): removed echo for format script

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-set -e
-
-if ! command -v clang-format &> /dev/null; then
-    echo "Error: clang-format not found. Install it with: brew install clang-format"
-    exit 1
-fi
-
 find src -name "*.cpp" -o -name "*.h" | xargs clang-format -i
