ef11563bb59d1ef29268ac5c8133b77063cf7363
Oliwier <m0lc14kk@outlook.com>
2025-12-09 18:47:37 +0100

    fix(models->password): fixed model converting in password conventer

--- src/database/services/passwords/PasswordsConverter.cpp
+++ src/database/services/passwords/PasswordsConverter.cpp
@@ -1,11 +1,11 @@
 #include "PasswordsConverter.h"
-#include "../../../globals/managers//security//EncryptionManager.h"
+#include "../../../globals/managers/security/EncryptionManager.h"
 
 RawPassword PasswordsConverter::convertToRawPassword(const Password& password) {
     RawPassword rawPassword;
     rawPassword.id = password.id;
     rawPassword.accountId = password.accountId;
-    rawPassword.name = password.name;
+    rawPassword.title = password.title;
     rawPassword.url = password.url;
     rawPassword.encryptedPassword = EncryptionManager::encrypt(password.password);
     rawPassword.encryptedSecurityPin =
@@ -17,7 +17,7 @@ Password PasswordsConverter::convertToPassword(const RawPassword& rawPassword) {
     Password password;
     password.id = rawPassword.id;
     password.accountId = rawPassword.accountId;
-    password.name = rawPassword.name;
+    password.title = rawPassword.title;
     password.url = rawPassword.url;
     password.password = EncryptionManager::decrypt(rawPassword.encryptedPassword);
     password.securityPin = std::stoi(EncryptionManager::decrypt(rawPassword.encryptedSecurityPin));
